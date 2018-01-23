f921bb70ce93a4f034798653c54a0cc33006ecb4
AK <35343810+andykintner@users.noreply.github.com>
2018-01-23 16:59:36 -0500

    Minesweeper lesson 3
    Third version of the code academy minesweeper lesson.

--- minesweeper.js
+++ minesweeper.js
@@ -1,20 +1,48 @@
-//Minesweeper Project Iteration 2
+//Minesweeper Project Iteration 3
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for(let rowCounter = 0; rowCounter < numberOfRows; rowCounter++) {
+    const row = [];
+    for(let columnCounter = 0; columnCounter < numberOfColumns; columnCounter++){
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for(let rowCounter = 0; rowCounter < numberOfRows; rowCounter++) {
+    const row = [];
+    for(let columnCounter = 0; columnCounter < numberOfColumns; columnCounter++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced < numberOfBombs){
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    /*
+    The code in your while loop has the potential to place bombs
+    on top of already existing bombs. This will be fixed when you
+    learn about control flow.
+    */
+  }
+  return board;
+};
 
 const printBoard = (board) => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 };
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
 
-console.log(printBoard(board));
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-console.log(printBoard(board));
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
